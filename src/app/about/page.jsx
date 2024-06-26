2078b5716404df57416ec20a882a0d9c51edfa61
Mohammad Salim <mohammadsalim017427@gmail.com>
2024-06-25 23:30:27 -0700

    SSs

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -4,18 +4,18 @@ export const metadata = {
     title: "about ",
     description: "CHEAPEST HERO",
   };
-  const getTime = async()=>{
-    const res = await fetch(`${process.env.URL}/time`,{next:{revalidate:5}});
-    const data = await res.json();
-    return data.currentTime;
-  }
+  // const getTime = async()=>{
+  //   const res = await fetch(`${process.env.URL}/time`,{next:{revalidate:5}});
+  //   const data = await res.json();
+  //   return data.currentTime;
+  // }
 const page =async () => {
-    const currentTime = await getTime();
+    // const currentTime = await getTime();
     return (
         <div>
            <h2>ABOUT</h2> 
            <AboutContent></AboutContent>
-           <h3 className="text-3xl text-red-400 mt-12">Time: {currentTime}</h3>
+           {/* <h3 className="text-3xl text-red-400 mt-12">Time: {currentTime}</h3> */}
         </div>
     );
 };
